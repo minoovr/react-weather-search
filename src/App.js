9470497c5329934e6c722acf4db1b52226047348
minoo <minoovahedi23@gmail.com>
2022-05-27 14:24:26 +0430

    added the Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/minoovr/react-weather-search"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+        , Coded by Minoo Vahedi Raad{" "}
+      </footer>
+    </div>
+  );
 }
